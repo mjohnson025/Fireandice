bc046b4e77fcfc1462a8d53ccd42e7dfd36f48e9
mjohnson025 <mjohnson025@msn.com>
2020-07-07 14:08:30 -0400

    Add files via upload

---
+++ Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    
+    public GameObject losePanel;
+    public Text healthDisplay;
+    public float speed;
+    private float input;
+    [SerializeField]
+    private float health = 0f;
+
+    public float startDashTime;
+    [SerializeField]
+    private float dashTime;
+    public float extraSpeed;
+    private bool isDashing;
+
+    Rigidbody2D rb;
+    Animator anim; 
+    AudioSource source;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+        healthDisplay.text = health.ToString();
+        
+    }
+
+    private void Update()
+    {
+        if (input != 0) {
+            anim.SetBool("isRunning", true);
+        }
+        else {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (input > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (input < 0) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        //Add dash ability
+        if(Input.GetKeyDown(KeyCode.Space) && isDashing == false){
+            speed += extraSpeed;
+            isDashing = true;
+            dashTime = startDashTime;
+            print("Space pressed");
+        }
+        if(dashTime <= 0 && isDashing == true) {
+            isDashing = false;
+            speed -= extraSpeed;
+        }
+        else{
+            dashTime -= Time.deltaTime;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+        print("Velocity: " + rb.velocity);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        source.Play();
+        healthDisplay.text = health.ToString();
+        if (health <= 0){
+            losePanel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] hazards;
+    private float timeBetweenSpawns;
+    public float startBetweenSpawns;
+    public float minTimeBetweenSpawns;
+    public float decrease;
+    public GameObject player;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null){
+
+            if(timeBetweenSpawns <= 0){
+                //Spawn Hazard
+                Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            // print(randomSpawnPoint);
+                GameObject randomHazard = hazards[Random.Range(0, hazards.Length)];
+                print(randomHazard);
+
+                Instantiate(randomHazard, randomSpawnPoint.position, Quaternion.identity);
+
+                if (startBetweenSpawns > minTimeBetweenSpawns){
+                    startBetweenSpawns -=decrease;
+                }
+                timeBetweenSpawns = startBetweenSpawns;
+                //print(timeBetweenSpawns);
+            }
+            else
+            {
+            timeBetweenSpawns -= Time.deltaTime;   
+            }
+
+        }
+    }
+}
